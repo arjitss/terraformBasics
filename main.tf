2a5f762f46f70b110daa0857656cfaa14b9973f7
Arjit <arjit.sharma@gmail.com>
2021-05-10 22:44:18 +0100

    Adding a new resource group & destroying it

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "weberver_rg" {
+  name     = "webserver-rg"
+  location = "westus2"
+}
+
+// commands used
+// az login - login to azure so as to connect where to create the resource
+// terraform init - initialise terraform
+// terraform fmt - to formate the code
+// terraform plan - generates the state, what actually will get deployed
+// terraform apply - to deploy the generated state to azure
+// terraform destroy - to destroy the generated state
