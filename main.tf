b5f320794b17bf52c9ee32c280bdbbd8fd13a0c6
Arjit <arjit.sharma@gmail.com>
2021-05-16 14:52:02 +0100

    Create VNet and Subnet

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ resource "azurerm_resource_group" "webserver_rg" {
   location = var.web_server_location
 }
 
+resource "azurerm_virtual_network" "web_server_vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.webserver_rg.name
+  address_space       = [var.web_server_address_space]
+}
+
+resource "azurerm_subnet" "web_server_subnet" {
+  name                 = "${var.resource_prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.webserver_rg.name
+  virtual_network_name = azurerm_virtual_network.web_server_vnet.name
+  address_prefix       = var.web_server_address_prefix
+}
+
 // commands used
 // az login - login to azure so as to connect where to create the resource
 // terraform init - initialise terraform
--- variables.tf
+++ variables.tf
@@ -1,10 +1,24 @@
 variable "web_server_location" {
-    type = string
-    description = "web server location"
+  type        = string
+  description = "web server location"
 }
 
 variable "web_server_rg" {
-    type = string
-    description = "resource group name"
+  type        = string
+  description = "resource group name"
 }
 
+variable "resource_prefix" {
+  type        = string
+  description = "resource prefix"
+}
+
+variable "web_server_address_space" {
+  type        = string
+  description = "web_server_address_space"
+}
+
+variable "web_server_address_prefix" {
+  type        = string
+  description = "web_server_address_prefix"
+}
