8758c5a14be35fcf5b9fb2fb2f2ac1fcc9ef6097
Arjit <arjit.sharma@gmail.com>
2021-06-05 22:59:39 +0530

    Adding a new Windows VM to resource group

--- main.tf
+++ main.tf
@@ -38,6 +38,8 @@ resource "azurerm_network_interface" "web_server_nic" {
     name                          = "${var.web_server_name}-ip"
     subnet_id                     = azurerm_subnet.web_server_subnet.id
     private_ip_address_allocation = "dynamic"
+    // Adding public ip to NIC
+    public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
   }
 }
 
@@ -51,7 +53,7 @@ resource "azurerm_public_ip" "webserver_public_ip" {
 resource "azurerm_network_security_group" "webserver_nsg" {
   name                = "${var.resource_prefix}-nsg"
   location            = var.web_server_location
-    // By using this instead of (var.web_server_rg) we are creating, 
+  // By using this instead of (var.web_server_rg) we are creating, 
   // a soft dependency and waiting for resource group to be created before creating NSG
   resource_group_name = azurerm_resource_group.webserver_rg.name
 }
@@ -75,6 +77,29 @@ resource "azurerm_network_interface_security_group_association" "webserver_nsg_a
   network_interface_id = azurerm_network_interface.web_server_nic.id
 }
 
+resource "azurerm_windows_virtual_machine" "webserver_vm" {
+  name = var.web_server_name
+  location = var.web_server_location
+  resource_group_name = azurerm_resource_group.webserver_rg.name
+  size = "Standard_B1s"
+  // VM is associated with NIC (network interface card) and 
+  // NIC is associated with public IP.
+  network_interface_ids = [azurerm_network_interface.web_server_nic.id]
+
+  admin_username = "webserver"
+  admin_password = "Passw0rd12345"
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer = "WindowsServer"
+    sku = "2019-Datacenter"
+    version = "latest"
+  }
+}
+
 // commands used
 // az login - login to azure so as to connect where to create the resource
 // terraform init - initialise terraform
@@ -83,3 +108,5 @@ resource "azurerm_network_interface_security_group_association" "webserver_nsg_a
 // terraform apply - to deploy the generated state to azure
 // terraform destroy - to destroy the generated state
 // terraform graph - to create a dependency graph ( Web site : https://dreampuf.github.io/GraphvizOnline/)
+// az vm list -o table
+// az vm list-sizes -l westus2 -o table
